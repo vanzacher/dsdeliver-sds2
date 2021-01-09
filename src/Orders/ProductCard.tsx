174695641f5977caeb86c5303ca4a84bab238448
unknown <van.zacher@gmail.com>
2021-01-09 12:02:28 -0300

    Search order location

---
+++ src/Orders/ProductCard.tsx
@@ -0,0 +1,42 @@
+import { format } from 'path';
+import { Product } from './types';
+
+type Props = {
+    product: Product;
+}
+
+function formatPrice(price: number) {
+    const formatter = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2
+    });
+
+    return formatter.format(price);
+}
+function ProductCard({ product }: Props) {
+    return (
+        <div className="order-card-container">
+            <h3 className="order-card-title">
+                {product.name}
+            </h3>
+            <img 
+                src={product.imageUri} 
+                className="order-card-image" 
+                alt={product.name}
+                />
+            <h3 className="order-cad-price">
+                {formatPrice(product.price)}
+            </h3>
+            <div className="order-card-dercription">
+                <h3>Dercrição</h3>
+                <p>
+                    {product.description}
+                </p>
+
+            </div>
+        </div>
+    )
+}
+
+export default ProductCard;
