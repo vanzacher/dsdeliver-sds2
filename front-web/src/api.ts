b4656ad3a1acfdc5b288c91f7c6e38b7fb4483fd
unknown <van.zacher@gmail.com>
2021-01-10 14:06:41 -0300

    Fix error deploy

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { OrderLocationData, OrderPayload } from "./Orders/types";
+import { OrderPayload } from "./Orders/types";
 
 const API_URL = 'http://localhost:8080'
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
